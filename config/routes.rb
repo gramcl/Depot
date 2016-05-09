274f070a3de96408f047671859151ca32ebe4d18
Graham <gmclellan3007@gmail.com>
2016-05-09 20:25:45 +0100

    finish off orders. Add atom feed

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'store#index', as: 'store'
 
+  resources :products do
+    get :who_bought, on: :member
+  end
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
