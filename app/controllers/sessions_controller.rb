5b0ff35fbd0ea7411085b6085026f951b1da75e5
Graham <gmclellan3007@gmail.com>
2016-05-15 19:17:57 +0100

    Complete authorization.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,13 @@
 class SessionsController < ApplicationController
+  skip_before_action :authorize
+  
   def new
   end
 
   def create
   	user = User.find_by(name: params[:name])
   	if user and user.authenticate(params[:password])
-  		session[:user_id] = user.user_id
+  		session[:user_id] = user.id
   		redirect_to admin_url
   	else
   		redirect_to login_url, alert: "Invalid user/password combination"
@@ -13,5 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+  	session[:user_id] = nil
+  	redirect_to store_url, notice: "Logged out"
   end
 end
