abb5bf7743fe975a025614c4816cc50e974fe7a2
Graham <gmclellan3007@gmail.com>
2016-05-16 21:26:13 +0100

    start translation stuff

--- config/routes.rb
+++ config/routes.rb
@@ -8,17 +8,21 @@ Rails.application.routes.draw do
 
 
   resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
-  get 'store/index'
+
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    get 'store/index'
+     # You can have the root of your site routed with "root"
+    root 'store#index', as: 'store', via: :all
+  end
 
   resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  root 'store#index', as: 'store'
+ 
 
   resources :products do
     get :who_bought, on: :member
